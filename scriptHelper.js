38851fd7b27ea3d6623b0bf8d8a97043152a0d8f
saranell <76405224+saranell@users.noreply.github.com>
2022-07-08 14:37:56 -0500

    Adding directions for step 1

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,9 +1,22 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+// validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate.
+// In scriptHelper.js, you will use validateInput() to complete the formSubmission() function.
+// formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass.
+// Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below.
+// Make sure to call your formSubmission() function at the appropriate time in your script.js file!
+require("isomorphic-fetch");
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+function addDestinationInfo(
+  document,
+  name,
+  diameter,
+  star,
+  distance,
+  moons,
+  imageUrl
+) {
+  // Here is the HTML formatting for our mission target div.
+  /*
                 <h2>Mission Destination</h2>
                 <ol>
                     <li>Name: </li>
@@ -16,28 +29,29 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
-   
-}
+function validateInput(testInput) {}
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+function formSubmission(
+  document,
+  list,
+  pilot,
+  copilot,
+  fuelLevel,
+  cargoLevel
+) {}
 
 async function myFetch() {
-    let planetsReturned;
+  let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+  planetsReturned = await fetch().then(function (response) {});
 
-    return planetsReturned;
+  return planetsReturned;
 }
 
-function pickPlanet(planets) {
-}
+function pickPlanet(planets) {}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
