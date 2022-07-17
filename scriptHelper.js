aa88d4fd3a4b81bd89107a6978b87c8b49b3c087
saranell <76405224+saranell@users.noreply.github.com>
2022-07-16 20:52:54 -0500

    Starting to work on json

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,8 +1,4 @@
 // Write your helper functions here!
-// TODO: validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate.
-// TODO: formSubmission() will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass.
-// TODO: Using the values in those strings and the document parameter for your HTML document, update the shuttle requirements as described below.
-// TODO: Make sure to call your formSubmission() function at the appropriate time in your script.js file!
 
 // require("isomorphic-fetch");
 
@@ -45,53 +41,68 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
   let launchStatus = document.getElementById("launchStatus");
   let fuelStatus = document.getElementById("fuelStatus");
   let cargoStatus = document.getElementById("cargoStatus");
-  //   let visibility = document.querySelector("visibility");
-  //TODO: Using template literals, update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
+
   validateInput(pilot.value, copilot.value);
-  if (pilot.value === "" || copilot === "") {
+  if (
+    pilot.value === "" ||
+    copilot === "" ||
+    fuelLevel === "" ||
+    cargoLevel === ""
+  ) {
     alert("All fields required!");
   }
+
   pilotStatus.innerHTML = `Pilot ${pilot.value} ready`;
   copilotStatus.innerHTML = `Co-pilot ${copilot.value} ready`;
 
   //TODO: If the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible with an updated fuel status stating that there is not enough fuel for the journey.
   // The text of the h2 element, launchStatus, should also change to "Shuttle not ready for launch" and the color should change to red.
   validateInput(fuelLevel);
-  if (!isNaN(fuelLevel)) {
-    alert("Invalid entry. Needs to be a number");
-    if (fuelLevel.value < 10000) {
-      list.style.visibility = "visible";
-      fuelStatus.innerHTML = "Not enough fuel for the journey!";
-      launchStatus.innerHTML = "Shuttle not ready for launch.";
-      launchStatus.style.color = "red";
-    }
-  }
-  //TODO: If the user submits a cargo mass that is too large (more than 10,000 kilograms), change the list to visible with an updated cargo status stating that there is too much mass for the shuttle to take off.
-  // The text of launchStatus should also change to "Shuttle not ready for launch" and the color should change to red.
-  validateInput(cargoLevel);
-  if (cargoLevel > 10000) {
+  //   if (!isNaN(fuelLevel)) {
+  //     alert("Invalid entry. Needs to be a number");
+  if (fuelLevel.value < 10000) {
     list.style.visibility = "visible";
-    cargoStatus.innerHTML = "Too much mass for takeoff!";
+    fuelStatus.innerHTML = "Not enough fuel for the journey!";
     launchStatus.innerHTML = "Shuttle not ready for launch.";
     launchStatus.style.color = "red";
-  } else if (!isNaN(cargoLevel)) {
-    alert("Invalid entry. Needs to be a number");
-  }
-  //TODO: If the shuttle is ready to launch, change the text of launchStatus to green and display "Shuttle is ready for launch".
-  if (fuelLevel.value >= 10000 && cargoLevel <= 10000) {
-    launchStatus.style.color = "green";
-    launchStatus.innerHTML = "Shuttle is ready for launch.";
   }
 }
+//TODO: If the user submits a cargo mass that is too large (more than 10,000 kilograms), change the list to visible with an updated cargo status stating that there is too much mass for the shuttle to take off.
+// The text of launchStatus should also change to "Shuttle not ready for launch" and the color should change to red.
+validateInput(cargoLevel);
+if (cargoLevel > 10000) {
+  list.style.visibility = "visible";
+  cargoStatus.innerHTML = "Too much mass for takeoff!";
+  launchStatus.innerHTML = "Shuttle not ready for launch.";
+  launchStatus.style.color = "red";
+} else if (!isNaN(cargoLevel)) {
+  alert("Invalid entry. Needs to be a number");
+}
+//TODO: If the shuttle is ready to launch, change the text of launchStatus to green and display "Shuttle is ready for launch".
+if (
+  fuelLevel.value >= 10000 &&
+  cargoLevel <= 10000 &&
+  pilotStatus !== "" &&
+  copilotStatus !== ""
+) {
+  launchStatus.style.color = "green";
+  launchStatus.innerHTML = "Shuttle is ready for launch.";
+}
+// }
 async function myFetch() {
   let planetsReturned;
 
-  planetsReturned = await fetch().then(function (response) {});
+  planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+    response.json().then( function(json) {
+    });
+  });
 
   return planetsReturned;
 }
 
-function pickPlanet(planets) {}
+function pickPlanet(planets) {
+    return Math.random();
+}
 
 // module.exports.addDestinationInfo = addDestinationInfo;
 // module.exports.validateInput = validateInput;
