9f0ac49686a1f5fd986ca773ba851fef1a59df78
saranell <76405224+saranell@users.noreply.github.com>
2022-07-16 21:11:42 -0500

    Started json

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ window.addEventListener("load", function () {
   let copilot = document.querySelector("input[name=copilotName");
   let fuelLevel = document.querySelector("input[name=fuelLevel]");
   let cargoLevel = document.querySelector("input[name=cargoMass]");
-    let listedPlanets;
+  let listedPlanets;
   //   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch();
-    listedPlanetsResponse
-      .then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-      })
-      .then(function () {
-        console.log(listedPlanets);
-  //       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-      });
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      //       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    });
   let form = document.querySelector("form");
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-    
   });
 });
