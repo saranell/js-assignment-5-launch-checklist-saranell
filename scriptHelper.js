c063411bd9aa8fac58d975cfb0fcd36acaca490b
saranell <76405224+saranell@users.noreply.github.com>
2022-07-24 20:39:28 -0500

    Calling validateInput() for each input

--- scriptHelper.js
+++ scriptHelper.js
@@ -40,41 +40,42 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
   let fuelStatus = document.getElementById("fuelStatus");
   let cargoStatus = document.getElementById("cargoStatus");
 
-  validateInput(pilot);
-  validateInput(copilot);
-
-  if (pilot === "" || copilot === "" || fuelLevel === "" || cargoLevel === "") {
+  if (
+    validateInput(pilot) === "Empty" ||
+    validateInput(copilot) === "Empty" ||
+    validateInput(fuelLevel) === "Empty" ||
+    validateInput(cargoLevel) === "Empty"
+  ) {
     alert("All fields required!");
-  } else if (!isNaN(pilot) || !isNaN(copilot)) {
+  } else if (!isNaN(validateInput(pilot)) || !isNaN(validateInput(copilot))) {
     alert("Invalid entry");
-  } else if (isNaN(fuelLevel) || isNaN(cargoLevel)) {
+  } else if (
+    isNaN(validateInput(fuelLevel)) ||
+    isNaN(validateInput(cargoLevel))
+  ) {
     alert("Invalid entry");
   } else {
     list.style.visibility = "visible";
     pilotStatus.innerHTML = `Pilot ${pilot} ready`;
     copilotStatus.innerHTML = `Co-pilot ${copilot} ready`;
-  }
-  validateInput(fuelLevel);
-  validateInput(cargoLevel);
-  if (fuelLevel < 10000) {
-    list.style.visibility = "visible";
-    fuelStatus.innerHTML = "Not enough fuel for the journey!";
-    launchStatus.innerHTML = "Shuttle not ready for launch.";
-    launchStatus.style.color = "red";
-  } else if (cargoLevel > 10000) {
-    list.style.visibility = "visible";
-    cargoStatus.innerHTML = "Too much mass for takeoff!";
-    launchStatus.innerHTML = "Shuttle not ready for launch.";
-    launchStatus.style.color = "red";
-    //   } else if (fuelLevel < 10000 && cargoLevel > 10000) {
-    //     list.style.visibility = "visible";
-    //     fuelStatus.innerHTML = "Not enough fuel for the journey!";
-    //     cargoStatus.innerHTML = "Too much mass for takeoff!";
-    //     launchStatus.innerHTML = "Shuttle not ready for launch.";
-    //     launchStatus.style.color = "red";
-  } else {
-    launchStatus.style.color = "green";
-    launchStatus.innerHTML = "Shuttle is ready for launch.";
+
+    validateInput(fuelLevel);
+    validateInput(cargoLevel);
+    if (fuelLevel < 10000 && cargoLevel > 10000) {
+      list.style.visibility = "visible";
+      fuelStatus.innerHTML = "Not enough fuel for the journey!";
+      cargoStatus.innerHTML = "Too much mass for takeoff!";
+      launchStatus.innerHTML = "Shuttle not ready for launch.";
+      launchStatus.style.color = "red";
+    } else if (cargoLevel > 10000) {
+      list.style.visibility = "visible";
+      cargoStatus.innerHTML = "Too much mass for takeoff!";
+      launchStatus.innerHTML = "Shuttle not ready for launch.";
+      launchStatus.style.color = "red";
+    } else {
+      launchStatus.style.color = "green";
+      launchStatus.innerHTML = "Shuttle is ready for launch.";
+    }
   }
 }
 async function myFetch() {
@@ -93,8 +94,8 @@ function pickPlanet(planets) {
   return planets[Math.floor(Math.random() * planets.length)];
 }
 
-// module.exports.addDestinationInfo = addDestinationInfo;
-// module.exports.validateInput = validateInput;
-// module.exports.formSubmission = formSubmission;
-// module.exports.pickPlanet = pickPlanet;
-// module.exports.myFetch = myFetch;
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;
